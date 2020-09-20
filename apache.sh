c432894007fa428290a2cf11b6264b58c6a17c9a
Surya <63136026+suraj687@users.noreply.github.com>
2020-09-20 06:40:28 +0530

    Update apache.sh

--- apache.sh
+++ apache.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
 
-sudo echo "how to install httpd with shell script"
-sudo yum install httpd -y
-sudo systemctl start httpd
-sudo echo "hello world" > /var/www/html/index.html
-sudo firewall-cmd --permanent --add-service=http
-sudo firewall-cmd --reload
+sudo -S echo "how to install httpd with shell script"
+sudo -S yum install httpd -y
+sudo -S systemctl start httpd
+sudo -S echo "hello world" > /var/www/html/index.html
+
 
